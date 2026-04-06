458dc2755c38ebabbab0580193a406ad54ea329e
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-04-06 19:55:27 +0900

    module ignored list

--- merge.sh
+++ merge.sh
@@ -5,4 +5,20 @@ dropout_files=(
   'archmba9.txt'
 )
 
-sort -k 1,1 "${dropout_files[@]}" | uniq > merged.txt
+# FIXME: per-machine blacklist
+dropout_ignores=(
+  'ac97_bus'
+  'snd_pcm_dmaengine'
+)
+
+function dropout_merge() {
+  sort -k 1,1 "${dropout_files[@]}" | uniq
+}
+
+function dropout_filter() {
+  echo "${dropout_ignores[*]}" | sed -e 's/^/^(/' -e 's/ /|/g' -e 's/$/)$/'
+}
+
+dropout_merge | grep -Ev "$(dropout_filter)" > merged.txt
+
+echo "W: excluding the following modules: ${dropout_ignores[*]}" >&2
